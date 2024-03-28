24cbdf8e42e8c54af69f7148da34759308468bfe
HoCG <rhwkd5012@naver.com>
2024-03-29 00:26:17 +0900

    feat: 라우터 기초구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
 import GlobalStyle from '@/styles/GlobalStyles';
 
 export default function App() {
@@ -10,14 +7,7 @@ export default function App() {
   return (
     <>
       <GlobalStyle />
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
