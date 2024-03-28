298a450fe94b3534155b6e20df46f8d753ebb5f2
HoCG <rhwkd5012@naver.com>
2024-03-29 00:18:54 +0900

    init: 구현에 필요한 라이브러리 및 스타일 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import GlobalStyle from '@/styles/GlobalStyles';
 
-function App() {
+export default function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <GlobalStyle />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -27,5 +29,3 @@ function App() {
     </>
   );
 }
-
-export default App;
